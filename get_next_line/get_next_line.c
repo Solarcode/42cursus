5080cdbe498ad63d387b0bb7353743b44f08fe1a
Solarcode <power@htb.com.au>
2022-02-11 15:47:30 +1030

    total overhaul

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,46 +6,124 @@
 /*   By: thardy <thardy@student.42adel.org.au>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/02 18:43:06 by thardy            #+#    #+#             */
-/*   Updated: 2022/02/10 11:50:45 by thardy           ###   ########.fr       */
+/*   Updated: 2022/02/11 15:45:25 by thardy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+char	*fill_remainder(int fd, char *remainder)
 {
-	static char	buf[BUFFER_SIZE + 1];
-	char		*line;
-	int			read_bytes;
+	char	*buf;
+	int		read_bytes;
 
-	line = 0;
 	read_bytes = 1;
-	while (!ft_strchr(buf, '\n') && read_bytes > 0)
+	buf = malloc(BUFFER_SIZE + 1);
+	if (!buf)
+		return (NULL);
+	while (!ft_strchr(remainder, '\n') && read_bytes != 0)
 	{
 		read_bytes = read(fd, buf, BUFFER_SIZE);
-		if (read_bytes > 0)
-			buf[read_bytes] = 0;
-				printf("buf = %s\n", buf);
-		line = get_line(buf, line);
-				printf("line: %s", line);
+		if (read_bytes == -1)
+		{
+			free(buf);
+			return (NULL);
+		}
+		buf[read_bytes] = 0;
+		remainder = ft_strjoin(remainder, buf);
 	}
-	return (line);
+	free(buf);
+	return (remainder);
 }
 
+char	*get_line(char *remainder)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	if (!remainder[i])
+		return (NULL);
+	while (remainder[i] && remainder[i] != '\n')
+		i++;
+	str = malloc(i + 2);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (remainder[i] && remainder[i] != '\n')
+	{
+		str[i] = remainder[i];
+		i++;
+	}
+	if (remainder[i] == '\n')
+	{
+		str[i] = remainder[i];
+		i++;
+	}
+	str[i] = 0;
+	return (str);
+}
+
+char	*new_remainder(char *remainder)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	while (remainder[i] && remainder[i] != '\n')
+		i++;
+	if (!remainder[i])
+	{
+		free(remainder);
+		return (NULL);
+	}
+	str = malloc(ft_strlen(remainder) - i + 1);
+	if (!str)
+		return (NULL);
+	i++;
+	while (remainder[i])
+		str[j++] = remainder[i++];
+	str[j] = 0;
+	free(remainder);
+	return (str);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*remainder;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	remainder = fill_remainder(fd, remainder);
+	if (!remainder)
+		return (NULL);
+	line = get_line(remainder);
+	remainder = new_remainder(remainder);
+	return (line);
+}
+/*
 int	main(void)
 {
     FILE	*fp;
     int		fd;
 	int		iter;
+	// char	buf[] = "aasdjkghaskjdhbvjhdgafnmv,\n asdkasdgjhdfskaljgmndv.";
 
 	iter = 0;
     fp = fopen("lorem-ipsum.txt", "r");
 	fd = fileno(fp);
     printf("fd: %d\n\n", fd);
+	// printf("cut_buf: %s\n", cut_buf(buf, 5));
+	// printf("buf: %s\n", buf);
 	while (iter < 8)
     {
 		if (printf("%i %s\n", iter, get_next_line(fd)) == 1)
 			break;
 		iter++;
 	}
+	close(fd);
 }
+*/
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,127 +6,11 @@
 /*   By: thardy <thardy@student.42adel.org.au>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/02 18:59:48 by thardy            #+#    #+#             */
-/*   Updated: 2022/02/10 11:49:51 by thardy           ###   ########.fr       */
+/*   Updated: 2022/02/11 15:45:15 by thardy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-char	*get_line(char *buf, char *line)
-{
-	int	i;
-
-	i = 0;
-	while (buf[i] && buf[i] != '\n' && buf[i] != '\0')
-		i++;
-	if (line)
-		return(ft_strjoin(line, buf));
-	else
-		return(line = ft_substr(buf, 0, i));
-	// printf("line after substr assign: %s\n\n\n", line);
-}
-
-char	*ft_strchr(const char *str, int c)
-{
-	size_t	i;
-
-	i = 0;
-	if (c > 127)
-		return ((char *)str);
-	while (str[i] || (str[i] == 0 && c == 0))
-	{
-		if (str[i] == c)
-			return ((char *)str + i);
-		i++;
-	}
-	return (NULL);
-}
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*s2;
-	size_t	i;
-
-	i = 0;
-	if (start > ft_strlen(s))
-		s2 = malloc(1);
-	else if (len > ft_strlen(s + start))
-		s2 = malloc(ft_strlen(s + start) + 1);
-	else
-		s2 = malloc(len + 1);
-	if (!s2)
-		return (NULL);
-	if (start < ft_strlen(s))
-		while (s[start] && i < len)
-			s2[i++] = s[start++];
-	s2[i] = 0;
-	return (s2);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	char	*ret;
-	size_t	i;
-	size_t	j;
-
-	j = 0;
-	i = 0;
-	ret = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (!ret)
-		return (NULL);
-	while (s1[j])
-		ret[i++] = s1[j++];
-	j = 0;
-	while (s2[j])
-		ret[i++] = s2[j++];
-	ret[i] = 0;
-	return (ret);
-}
-
-int	findchar(const char *str, int c)
-{
-	size_t	i;
-
-	i = 0;
-	if (c > 127)
-		return (-1);
-	while (str[i] || (str[i] == 0 && c == 0))
-	{
-		if (str[i] == c)
-			return (i);
-		i++;
-	}
-	return (ft_strlen(str));
-}
-
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
-{
-	const char	*og_dst;
-	const char	*og_src;
-	size_t		n;
-	size_t		len;
-
-	og_dst = dst;
-	og_src = src;
-	n = size;
-	while (n-- && *dst)
-		dst++;
-	len = dst - og_dst;
-	n = size - len;
-	if (n-- == 0)
-		return (len + ft_strlen(src));
-	while (*src)
-	{
-		if (n)
-		{
-			*dst++ = *src;
-			n--;
-		}
-		src++;
-	}
-	*dst = 0;
-	return (len + (src - og_src));
-}
 
 size_t	ft_strlen(const char *str)
 {
@@ -138,32 +22,80 @@ size_t	ft_strlen(const char *str)
 	return (i);
 }
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+char	*ft_strchr(const char *str, int c)
 {
 	size_t	i;
-	size_t	c;
 
 	i = 0;
-	c = 0;
-	if (!dst || !src)
+	if (!str)
 		return (0);
-	while (src[c])
-		c++;
-	while (src[i] && i + 1 < size)
+	if (c > 127)
+		return ((char *)str);
+	if (c == 0)
+		return ((char *)str + ft_strlen(str));
+	while (str[i] != 0)
 	{
-		dst[i] = src[i];
+		if (str[i] == c)
+			return ((char *)str + i);
 		i++;
 	}
-	if (size)
-		dst[i] = 0;
-	return (c);
+	return (NULL);
 }
 
-void	line_cat(char *line, char *buf, int i)
+void	ft_bzero(void *s, size_t n)
 {
-	char	*tmp;
+	size_t	i;
+	char	*c;
 
-	tmp = ft_strjoin(line, ft_substr(buf, 0, i));
-	free(line);
-	line = tmp;
+	i = 0;
+	c = (char *) s;
+	while (i < n)
+	{
+		c[i] = '\0';
+		i++;
+	}
+}
+
+void	*ft_calloc(size_t nitems, size_t size)
+{
+	void	*p;
+	size_t	bytes;
+
+	if (nitems == 0 || size == 0)
+	{
+		nitems = 1;
+		size = 1;
+	}
+	bytes = nitems * size;
+	p = malloc(bytes);
+	if (p == NULL)
+		return (NULL);
+	ft_bzero(p, bytes);
+	return (p);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*ret;
+	size_t	i;
+	size_t	j;
+
+	j = 0;
+	i = 0;
+	if (!s1)
+		s1 = ft_calloc(1, sizeof(char));
+	if (!s1 || !s2)
+		return (NULL);
+	ret = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!ret)
+		return (NULL);
+	if (s1)
+		while (s1[j])
+			ret[i++] = s1[j++];
+	j = 0;
+	while (s2[j])
+		ret[i++] = s2[j++];
+	ret[i] = 0;
+	free((void *)s1);
+	return (ret);
 }
